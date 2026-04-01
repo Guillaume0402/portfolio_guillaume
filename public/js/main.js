69c8fca289674267a14ddd77d22396c2e78e7539
Guillaume0402 <g.maignaut@gmail.com>
2026-04-01 09:48:39 +0200

    refactor(js): rename toggle card module

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 import { initMenu } from "./modules/menu.js";
 import { initReveal } from "./modules/reveal.js";
 import { initCardsGlow } from "./modules/cardsGlow.js";
-import { initToggleCard } from "./modules/toogle-card.js";
+import { initToggleCard } from "./modules/toggle-card.js";
 
 function initUI() {
     initMenu();
