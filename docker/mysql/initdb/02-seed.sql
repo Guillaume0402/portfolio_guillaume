2b2be2d29c68f358f89dd0cde1ea99dd45fcb165
Guillaume0402 <g.maignaut@gmail.com>
2026-04-03 15:18:17 +0200

    fix(build): align Docker and Sass workflow for local and production

--- docker/mysql/initdb/02-seed.sql
+++ docker/mysql/initdb/02-seed.sql
@@ -1,9 +1,9 @@
 USE `portfolio`;
 
-INSERT INTO `projects` (`idproject`, `title`, `description`, `github_link`, `project_link`, `image`) VALUES
-(1, 'EcoRide', 'Plateforme de covoiturage basé sur l\'écologie', 'https://github.com/Guillaume0402/ecoride', 'https://ecoride-app-gn-39216337ff0d.herokuapp.com/', 'ecoride.png'),
-(2, 'Tickylist', 'Application de gestion de projet', 'https://github.com/Guillaume0402/TickyList', NULL, 'tutoPHP'),
-(3, 'TutoPHP', 'Site d\'apprentissage de PHP', 'https://github.com/Guillaume0402/Tuto-php', 'https://tuto-php-19982edf3ffe.herokuapp.com/', 'tuto-php.png');
+INSERT INTO `projects` (`idproject`, `title`, `description`, `github_link`, `project_link`, `image`, `tech_stack`) VALUES
+(1, 'EcoRide', 'Plateforme de covoiturage écologique full stack avec authentification sécurisée, système de crédits, gestion des trajets, rôles utilisateurs et notifications e-mail automatiques lors des demandes de participation et de leur validation.', 'https://github.com/Guillaume0402/ecoride', 'https://ecoride-app-gn-39216337ff0d.herokuapp.com/', 'ecoride.png', 'PHP, HTML, CSS, Docker, SQL, NoSQL'),
+(2, 'Tichylist', 'Application de gestion de tâches et de projets intégrant une authentification sécurisée, la création de comptes utilisateurs et un suivi structuré des listes et projets.', 'https://github.com/Guillaume0402/TickyList', NULL, 'tickylist.png', 'PHP, HTML, CSS, Docker, SQL'),
+(3, 'TutoPHP', 'Site d’apprentissage orienté PHP vanilla, créé pour rendre les notions essentielles plus accessibles à travers une présentation claire et des cas pratiques.', 'https://github.com/Guillaume0402/Tuto-php', 'https://tuto-php-19982edf3ffe.herokuapp.com/', 'tuto-php.png', 'PHP, HTML, CSS');
 
 INSERT INTO `skills` (`idskills`, `name`, `logo`) VALUES
 (1, 'PHP', 'phpsbg.png'),
@@ -13,4 +13,5 @@ INSERT INTO `skills` (`idskills`, `name`, `logo`) VALUES
 (5, 'SASS', 'sasssbg.png'),
 (6, 'SQL', 'sqlsbg.png'),
 (7, 'DOCKER', 'dockersbg.png'),
-(8, 'NOSQL', 'nosqlsbg.png');
+(8, 'NOSQL', 'nosqlsbg.png'),
+(9, 'LINUX', 'linuxsbg.png');
